e24b6a5c4316007852aa539dfbe8a718fa7ca2e5
Dmitry Mokshin <denih@list.ry>
2021-10-10 00:17:14 +0300

    Creating files 3

---
+++ Task_4/init_data.f90
@@ -0,0 +1,22 @@
+module init_data
+    implicit none
+    integer, parameter :: mp = 8, num_of_coef = 10
+    real(mp), parameter :: a = 0.0, b = 0.0
+contains
+
+    function kernal_integral_equation(x, t)
+        real(mp) :: x, t
+        real(mp) :: kernal_integral_equation
+
+            kernal_integral_equation = 1.0_mp / (x + 1.0_mp) ** 2 / (t + 1.0_mp) ** 2
+
+    end function kernal_integral_equation
+
+    function f(x) result(result_fun)
+        real(mp) :: x, result_fun
+
+            result_fun = log(2.0_mp * (x + 2.0_mp) / (2.0_mp * x + 3.0_mp)) - log(2.0_mp) / (x + 1.0_mp) / (2.0_mp * x + 3.0_mp)
+        
+    end function f
+
+end module init_data
