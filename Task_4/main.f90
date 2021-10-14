b2970c6c8e4c828b93802174cffd6c6b97bfb35a
Dmitry Mokshin <denih@list.ry>
2021-10-14 03:05:42 +0300

    Fix conclusion and delete analyses

--- Task_4/main.f90
+++ Task_4/main.f90
@@ -2,59 +2,61 @@ program main
     use :: init_data
     use :: SIEI
 implicit none
-	integer, parameter :: num_of_alphas = 8
+    integer, parameter :: num_of_alphas = 8
     integer :: i, n, k
     real(mp), dimension(0:8, 1:num_of_coef) :: c
     real(mp) :: h, x
     real(mp), dimension(0:num_of_alphas) :: alphas
-    
+    character(2) :: num_of_coef_char
+
     alphas = (/( 0.01_mp ** i, i = 0, num_of_alphas )/)
 
-	do i = 0, 8
-		c(i, :) = coefficients_of_the_series(kernel_T, f_1, alphas(i))
+    num_of_coef_char = char(ichar('0')+num_of_coef/10)//char(ichar('0')+mod(num_of_coef,10))
+
+    do i = 0, 8
+        c(i, :) = coefficients_of_the_series(kernel_T, f_1, alphas(i))
     end do 
 
     n = 20
     h = (b - a) / n
 
-    open(15, file='result.dat', status='replace')
-    
-    write(15, '(20x, a, $)') '|'
+    open(15, file='result'//num_of_coef_char//'.csv', status='replace')
+
+    write(15, '(a, $)') 'x'
     do i = 0, num_of_alphas
-		write(15, '(a, e9.2, 5x, a, $)') 'alpha = ', alphas(i), '|'
-	end do
-	
-	write(15, *) 
+        write(15, '(a, a, e9.2, $)') '|', 'y, alpha = ', alphas(i)
+    end do
+
+    write(15, *) 
 
     do i = 0, n
-		x = a + h * i
-		write(15, '(e20.12, a, $)') x, '|'  
-		do k = 0, num_of_alphas
-			write(15, '(e20.12, 2x, a, $)') result_fun(x, c(k, :)), '|'
-		end do
-		write(15,*)
+        x = a + h * i
+        write(15, '(e20.12, $)') x
+        do k = 0, num_of_alphas
+            write(15, '(a, e20.12, $)') '|', result_fun(x, c(k, :))
+        end do
+        write(15,*)
     end do
 
     close(15)
 
-    open(17, file='result_coef.dat', status='replace')
+    open(17, file='result_coef'//num_of_coef_char//'.csv', status='replace')
 
-    do i = 0, num_of_alphas
-		write(17, '(a, e9.2, 5x, a, $)') 'alpha = ', alphas(i), '|'
-	end do
-	
-	write(17, *) 
-    
-	do k = 1, num_of_coef
-		do i = 0, num_of_alphas
-			write(17, '(e20.12, 2x, a, $)') c(i, k), '|'
-		end do
-		write(17, *)
+    write(17, '(a, e9.2, $)') 'y, alpha = ', alphas(i)
+    do i = 1, num_of_alphas
+        write(17, '(a, e9.2, $)') '| c_i, alpha = ', alphas(i)
+    end do
+
+    write(17, *) 
+
+    do k = 1, num_of_coef
+        write(17, '(e20.12,  $)') c(0, k)
+        do i = 1, num_of_alphas
+            write(17, '(a, e20.12,  $)') '|', c(i, k)
+        end do
+        write(17, *)
     end do
 
     close(17)
-
-    !write(*, *) 'alpha = ', alphas
-    !write(*, *) error_method_collocations(c, kernal_integral_equation)
 
 end program main
